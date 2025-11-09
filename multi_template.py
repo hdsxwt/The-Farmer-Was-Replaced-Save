abdd9f4301e98b27647b181c7d9cd6e4308ead9c
hdsxwt <hdsxwt@outlook.com>
2025-11-09 18:07:44 +0800

    fix bug in multi_template

--- multi_template.py
+++ multi_template.py
@@ -1,12 +1,29 @@
 def commonRet():
-	return []
+	return [None]
+	# the position 0 is signal
 clear()
 drone = spawn_drone(commonRet)
+
+def f(id, shared):
+	print(id)
+	do_a_flip()
+	do_a_flip()
+	do_a_flip()
+	print(shared)
+
 def worker():
 	shared = wait_for(drone)
+	id = num_drones()-1
+	shared.append(id)
 	while True:
-		print(shared)
+		f(id, shared)
 
-for i in range(max_drones() - 1):
+for i in range(max_drones()-1):
 	spawn_drone(worker)
 	move(North)
+
+shared = wait_for(drone)
+id = num_drones()
+shared.append(id)
+while True:
+	f(id, shared)
--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-harvest()
+sim_items = {Items.Carrot:999999999, Items.Weird_Substance:9999999999}
+speedup = 1
+seed = 1
+sim_globals = {}
+res = []
+
+sim_unlock = {}
+
+for uk in Unlocks:
+	sim_unlock[uk] = 20
+	
+sim_unlock[Unlocks.Expand] = 5
+sim_unlock[Unlocks.Megafarm] = 2
+
+# modify
+k = 5
+
+for i in range(k):
+	seed = i * i
+	tmp = simulate("multi_template", sim_unlock, sim_items, sim_globals, seed, speedup)
+	res.append(tmp)
+	quick_print(tmp)
+
+sum = 0
+for i in range(k):
+	sum += res[i]
+print("ave" , sum / k)
