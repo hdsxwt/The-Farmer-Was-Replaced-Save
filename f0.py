1e9532f34d3a1e2ab01400b5fd1ee695e8e5baca
hdsxwt <hdsxwt@outlook.com>
2025-11-22 22:52:37 +0800

    make an achievement

--- f0.py
+++ f0.py
@@ -1,2 +1,8 @@
-from std import *
-for_all(harvest)
+def f():
+	if can_harvest():
+		pass
+	if get_entity_type() == Entities.Dead_Pumpkin or get_entity_type() == None:
+		plant(Entities.Pumpkin)
+from for_all import for_all
+
+for_all(f)
--- till_all.py
+++ till_all.py
@@ -1,8 +1,2 @@
-from more_move import *
-siz = get_world_size()
-for i in range(siz):
-	for j in range(siz):
-		if get_ground_type() == Grounds.Grassland:
-			till()
-		mov(1, 0)
-	mov(0, 1)
+from for_all import for_all
+for_all(till)
