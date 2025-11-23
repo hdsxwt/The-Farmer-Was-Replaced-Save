a44d90dc26ca701b4a4c5bca28200d7e0f305936
hdsxwt <hdsxwt@outlook.com>
2025-11-23 17:37:56 +0800

    perf(carrot): :zap:

--- carrot.py
+++ carrot.py
@@ -1,13 +1,39 @@
-from std import *
 
-def run_carrot(target=None, data=None):
-	def f():
-		try_plant(Grounds.Soil, Entities.Carrot)
-	move_to(0, 0)
-	for_all(f)
-	
-if __name__ == "__main__":
+def worker():
+	pcnt = 0
+	cnt = 0
+	deadcnt = 0
 	while True:
-		run_carrot()
+		cnt += 1
+		if get_pos_x() == 0 and get_pos_y() == 0:
+			pcnt += 1
+			if pcnt == 1:
+				quick_print(deadcnt/cnt)
+				cnt = 0
+				deadcnt = 0
+				pcnt = 0
+		
+		water_line = 0.30 # min 0.08
+		while (not can_harvest()) and get_entity_type() != None:
+			deadcnt += 30
+			if get_water() <= water_line:
+				use_item(Items.Water)
+		if get_water() <= water_line:
+			use_item(Items.Water)
+		harvest()
 
-# 17.7k / min
+		if get_ground_type() == Grounds.Grassland:
+			till()
+
+		plant(Entities.Carrot)
+		move(East)
+
+
+def run_carrot():
+	for i in range(get_world_size()):
+		if not spawn_drone(worker):
+			worker()
+		move(North)
+
+if __name__ == "__main__":
+	run_carrot()
