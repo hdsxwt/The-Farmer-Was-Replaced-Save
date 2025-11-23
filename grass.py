8f515b43ef2a1d28375e81cb21a9cd491fb191eb
hdsxwt <hdsxwt@outlook.com>
2025-11-23 16:51:17 +0800

    refactor: grass

--- grass.py
+++ grass.py
@@ -1,25 +1,16 @@
-from std import *
 
-def run_grass(target=None, data=None):
-	move_to(0, get_pos_y())
-	def run():
-		for j in range(get_world_size()):
+def run_grass():
+	clear()
+
+	def worker():
+		while True:
 			harvest()
-			if (get_ground_type()==Grounds.Soil):
-				till()
 			move(East)
-	for i in range(max_drones()):
-		if not spawn_drone(run):
-			run()
+
+	for i in range(get_world_size()):
+		if not spawn_drone(worker):
+			worker()
 		move(North)
-	
-				
+
 if __name__ == "__main__":
-	# set_execution_speed(1)
-	while True:
-		st = get_time()
-		run_grass()
-		quick_print(get_time() - st)
-		
-#ori 26.78s
-#new 17.54s
+	run_grass()
