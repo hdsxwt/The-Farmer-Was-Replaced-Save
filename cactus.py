266872103dd1194bef3169c56cbeb2a6f2989db7
hdsxwt <xuewentao3416@163.com>
2025-11-29 21:55:10 +0800

    feat(cactus): :zap: improve perrformance in multiple cactus

--- cactus.py
+++ cactus.py
@@ -3,19 +3,65 @@ from std import *
 def run_cactus(target=None, data=None):
 	clear()
 	siz = get_world_size()
-	def f():
+	def edit_in_line():
 		for j in range(siz):
 			try_plant(Grounds.Soil, Entities.Cactus)
 			move(East)
-		for j in range(siz-1):
-			move_to(0, i)
-			for k in range(siz-j-1):
-				if measure(East) < measure():
-					swap(East)
-				move(East)
+		move_to(1, i)
+		l, r = 0, siz-1
+		op = 1
+		while l < r:
+			if op == 1:
+				while get_pos_x() < r-1:
+					if measure(East) < measure():
+						swap(East)
+					if measure(West) > measure():
+						swap(West)
+					if measure(East) < measure():
+						swap(East)
+					move(East)
+				r -= 2
+			else:
+				while get_pos_x() > l+1:
+					if measure(East) < measure():
+						swap(East)
+					if measure(West) > measure():
+						swap(West)
+					if measure(East) < measure():
+						swap(East)
+					move(West)
+				l += 2
+			op = -op
+
+	def edit_in_row():
+		move_to(i, 1)
+		l, r = 0, siz-1
+		op = 1
+		while l < r:
+			if op == 1:
+				while get_pos_y() < r-1:
+					if measure(North) < measure():
+						swap(North)
+					if measure(South) > measure():
+						swap(South)
+					if measure(North) < measure():
+						swap(North)
+					move(North)
+				r -= 2
+			else:
+				while get_pos_y() > l+1:
+					if measure(North) < measure():
+						swap(North)
+					if measure(South) > measure():
+						swap(South)
+					if measure(North) < measure():
+						swap(North)
+					move(South)
+				l += 2
+			op = -op
 	for i in range(siz):
-		if not spawn_drone(f):
-			f()
+		if not spawn_drone(edit_in_line):
+			edit_in_line()
 		move(North)
 	
 	while num_drones() != 1:
@@ -23,16 +69,9 @@ def run_cactus(target=None, data=None):
 
 	move_to(0, 0)
 
-	def g():
-		for j in range(siz-1):
-			move_to(i, 0)
-			for k in range(siz-j-1):
-				if measure(North) < measure():
-					swap(North)
-				move(North)
 	for i in range(siz):
-		if not spawn_drone(g):
-			g()
+		if not spawn_drone(edit_in_row):
+			edit_in_row()
 		move(East)
 	
 	while num_drones() != 1:
@@ -40,4 +79,5 @@ def run_cactus(target=None, data=None):
 	harvest()
 				
 if __name__ == "__main__":
-	run_cactus()
+	while True:
+		run_cactus()
